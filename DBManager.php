cd0c041b83392222b20110ee64070db07009dd0c
EmmaMilani <94352149+EmmaMilani@users.noreply.github.com>
2024-01-10 20:34:03 +0100

    DBManager

---
+++ DBManager.php
@@ -0,0 +1,25 @@
+<?php
+class DBManager
+{
+    private $host, $port, $username, $password;
+    function costruttore($host, $port, $username, $password)
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->username = $username;
+        $this->password = $password;
+    }
+    public function Connect($dbname)
+    {
+        $dsn = "mysql:dbname={$dbname};host={$this->host}";
+
+        try {
+            $pdo = new PDO($dsn, $this->username, $this->password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Connessione al DB fallita: " . $e->getMessage());
+        }
+    }
+}
+?>
